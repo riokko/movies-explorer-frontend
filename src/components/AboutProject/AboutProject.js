cdfb9843845ca7f917bdd0e1361cd5f6943d720b
Daria Barova <breya@yandex.ru>
2021-04-26 17:28:15 +0300

    rename aboutProject in css-files

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -3,34 +3,34 @@ import Row from "../Row";
 
 function AboutProject() {
   return (
-    <div className="aboutProject">
+    <div className="about-project">
       <Row>
         <h2 className="main__title">О Проекте</h2>
-        <div className="aboutProject__content">
-          <div className="aboutProject_two-columns">
-            <h3 className="aboutProject_two-columns__title">
+        <div className="about-project__content">
+          <div className="about-project_two-columns">
+            <h3 className="about-project_two-columns__title">
               Дипломный проект включал 5 этапов
             </h3>
 
-            <h3 className="aboutProject_two-columns__title">
+            <h3 className="about-project_two-columns__title">
               На выполнение диплома ушло 5 недель
             </h3>
-            <p className="aboutProject_two-columns__text">
+            <p className="about-project_two-columns__text">
               Составление плана, работу над бэкендом, вёрстку, добавление
               функциональности и финальные доработки.
             </p>
-            <p className="aboutProject_two-columns__text">
+            <p className="about-project_two-columns__text">
               У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
               соблюдать, чтобы успешно защититься.
             </p>
           </div>
-          <div className="aboutProject_timeline">
-            <div className="aboutProject_timeline__1-week">1 неделя</div>
-            <div className="aboutProject_timeline__4-weeks">4 недели</div>
+          <div className="about-project_timeline">
+            <div className="about-project_timeline__1-week">1 неделя</div>
+            <div className="about-project_timeline__4-weeks">4 недели</div>
           </div>
-            <div className="aboutProject_timeline">
-                <div className="aboutProject_timeline__backend">Back-end</div>
-                <div className="aboutProject_timeline__frontend">Front-end</div>
+            <div className="about-project_timeline">
+                <div className="about-project_timeline__backend">Back-end</div>
+                <div className="about-project_timeline__frontend">Front-end</div>
             </div>
         </div>
       </Row>
