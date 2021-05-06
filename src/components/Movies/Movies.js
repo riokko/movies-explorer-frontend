d3389dcba0476274a3a1ee22ce9668e3ec3ef70c
Daria Barova <breya@yandex.ru>
2021-05-06 17:24:20 +0300

    fix width on adaptive

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -7,33 +7,6 @@ import MoviesCardList from "../MoviesCardList/MoviesCardList";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 
-// const movies = [
-//     {
-//         name: "33 слова о дизайне",
-//         duration: 107,
-//         like: true,
-//         thumbnail: "https://ic.wampi.ru/2021/05/06/first_movie.jpg",
-//     },
-//     {
-//         name: "Киноальманах «100 лет дизайна»",
-//         duration: 107,
-//         like: false,
-//         thumbnail: "https://ic.wampi.ru/2021/05/06/second_movie.jpg",
-//     },
-//     {
-//         name: "В погоне за Бенкси",
-//         duration: 102,
-//         like: true,
-//         thumbnail: "https://ic.wampi.ru/2021/05/06/third_movie.png",
-//     },
-//     {
-//         name: "Баския: Взрыв реальности",
-//         duration: 81,
-//         like: false,
-//         thumbnail: "https://ic.wampi.ru/2021/05/06/fourth_movie.png",
-//     },
-// ];
-
 function Movies({ loggedIn, movies, fetchData }) {
     return (
         <div className="movies">
@@ -41,9 +14,16 @@ function Movies({ loggedIn, movies, fetchData }) {
             <Row>
                 <SearchForm fetchData={fetchData} />
                 <MoviesCardList movies={movies} />
-                <button className="movies__more-button" type="button">
-                    Ещё
-                </button>
+                {movies.length > 0 ? (
+                    <button className="movies__more-button" type="button">
+                        Ещё
+                    </button>
+                ) : (
+                    <p>
+                        Введите название или ключевые слова в строку поиска.
+                    </p>
+                )}
+
                 <Footer />
             </Row>
         </div>
