95b526f4bed9b284aef2cd55b22f95ab910e10d0
Daria Barova <breya@yandex.ru>
2021-05-21 16:39:08 +0300

    delete comments from code

--- src/components/MoviesCardList/MoviesCardList.js
+++ src/components/MoviesCardList/MoviesCardList.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import { useLocation } from "react-router-dom";
 import MoviesCard from "../MoviesCard/MoviesCard";
 import "./MoviesCardList.css";
 import { BASE_URL } from "../../utils/MoviesApi";
@@ -12,9 +11,6 @@ function MoviesCardList({
     likedMovies,
     fetchLikedMovies,
 }) {
-    // const [isLiked, setIsLiked] = useState(true);
-    // const currentLocation = useLocation();
-    // const path = currentLocation.pathname;
     if (!movies) {
         return null;
     }
@@ -43,48 +39,6 @@ function MoviesCardList({
             </ul>
         </div>
     );
-
-    // return (
-    //     <div className="movies-cardlist">
-    //         <ul className="movies-cardlist__list">
-    //             {path === "/movies"
-    //                 ? movies.map((movie) => {
-    //                       const imageUrl = movie.image
-    //                           ? `${BASE_URL}${movie.image.url}`
-    //                           : noCover;
-    //                       return (
-    //                           <MoviesCard
-    //                               key={movie.id}
-    //                               movie={movie}
-    //                               isSavedPage={isSavedPage}
-    //                               setLikedMovies={setLikedMovies}
-    //                               likedMovies={likedMovies}
-    //                               fetchLikedMovies={fetchLikedMovies}
-    //                               image={imageUrl}
-    //                               isLiked={isLiked}
-    //                               setIsLiked={setIsLiked}
-    //                           />
-    //                       );
-    //                   })
-    //                 : likedMovies.map((movie) => {
-    //                     console.log(movie)
-    //                     return <MoviesCard
-    //                         key={movie.movieId}
-    //                         movie={movie}
-    //                         isSavedPage={isSavedPage}
-    //                         setLikedMovies={setLikedMovies}
-    //                         likedMovies={likedMovies}
-    //                         fetchLikedMovies={fetchLikedMovies}
-    //                         image={movie.image}
-    //                         isLiked={!isLiked}
-    //                         setIsLiked={setIsLiked}
-    //                     />
-    //                 })}
-    //
-    //             {}
-    //         </ul>
-    //     </div>
-    // );
 }
 
 export default MoviesCardList;
